f8f32e9b852f9f0bf88779888c594334f45f678b
LunaUI <59039458+LunaValerie@users.noreply.github.com>
2022-09-03 21:15:42 +0300

    Aim
    Aim

---
+++ aimbot.cpp
@@ -0,0 +1,5 @@
+mouse y : -1.39626348 - yerabak
+mouse x : 1.570796251 - oyundan gır cık 0 konumundayken float arat
+character y : 2.343393564 - kütüphane en kose sol
+character x : -3.031940699 - kütüphane en kose sol
+character z : -2.263900995 - kütüphane en kose sol
